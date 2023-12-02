948e466f4c24d454de1ac6c442b34de2a220f362
GREGORY BUSANELLO <gregorybusanello@gmail.com>
2023-12-02 12:53:37 +0000

    criada dim_produtos, atualizado o dbt_project.yml

--- models/staging/erp_northwind/stg_erp__produtos.sql
+++ models/staging/erp_northwind/stg_erp__produtos.sql
@@ -4,7 +4,6 @@ with
             cast(product_id as int) as id_produto
             , cast(supplier_id as int) as id_fornecedor
             , cast(category_id as int) as id_categoria
-            
             , cast(product_name as string) as nome_produto
             , cast(quantity_per_unit as string) as quantidade_por_unidade
             , cast(unit_price as numeric) preco_por_unidade
